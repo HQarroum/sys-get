f1ebb5929acbe5fe68e632ff503ce0da36d793da
Halim Qarroum <hqm.post@gmail.com>
2018-05-10 04:22:57 +0200

    Refactor application

--- index-serve.js
+++ index-serve.js
@@ -1,13 +1,105 @@
-const program = require('commander');
-const term    = require('terminal-kit').terminal;
-const dump    = require('./lib/dump');
-const storage = require('./lib/storage');
+const program     = require('commander');
+const Expressify  = require('../expressify');
+const IpcStrategy = require('../expressify-ipc');
+const system      = require('./lib');
 
 /**
  * Command-line interface.
  */
 program
-  .option('-n, --namespace', 'The IPC namespace to use.')
-  .option('-b, --endpoint', 'The IPC endpoint to use.')
+  .option('-n, --namespace [namespace]', 'The IPC namespace to use.')
+  .option('-b, --endpoint [endpoint]', 'The IPC endpoint to use.')
   .parse(process.argv);
 
+/**
+ * The domains we are notifying the client about.
+ */
+const domains = [
+  'processes',
+  'cpu',
+  'storage',
+  'os',
+  'memory',
+  'network'
+];
+
+/**
+ * Creating the Expressify server.
+ */
+const server = new Expressify.Server({
+  strategy: new IpcStrategy({
+    endpoint: program.endpoint || 'monitoring.server',
+    namespace: program.namespace || 'system'
+  })
+});
+
+/**
+ * Retrieves and returns informations about running
+ * processes on the host, along with load information.
+ */
+server.get('/system/processes', (req, res) => {
+  system.processes.get().then((r) => res.send(r));
+});
+
+/**
+ * Retrieves and returns information about the avaialable CPUs,
+ * as well as load information.
+ */
+server.get('/system/cpu', (req, res) => {
+  system.cpu.get().then((r) => res.send(r));
+});
+
+/**
+ * Retrieves and returns information abut the storage
+ * devices available on the host.
+ */
+server.get('/system/storage', (req, res) => {
+  system.storage.get().then((r) => res.send(r));
+});
+
+/**
+ * Retrieves and returns general information about
+ * the host operating system.
+ */
+server.get('/system/os', (req, res) => {
+  system.os.get().then((r) => res.send(r));
+});
+
+/**
+ * Retrieves and returns information about the host
+ * memory (usage, load, free memory).
+ */
+server.get('/system/memory', (req, res) => {
+  system.memory.get().then((r) => res.send(r));
+});
+
+/**
+ * Retrieves and returns information on the network
+ * interfaces available on the host.
+ */
+server.get('/system/network', (req, res) => {
+  system.network.get().then((r) => res.send(r));
+});
+
+/**
+ * Listening for incoming requests.
+ */
+server.listen().then(() => {
+  console.log(`[+] The server is listening for incoming requests on namespace '${server.strategy.opts.namespace}' !`);
+  // Creating the interval loop notifying clients of
+  // changes in the local system model.
+  intervals = domains.map((d) => setInterval(() => {
+    system[d].get().then((r) => server.publish(`/system/${d}`, r));
+  }, 10 * 1000));
+});
+
+/**
+ * Closing the IPC connection when leaving the application.
+ */
+process.on('SIGINT', () => {
+  console.log('[+] Closing the IPC connection ...');
+  // Clears all created intervals.
+  intervals.forEach((i) => clearInterval(i));
+  // Closing the connection.
+  server.close().then(process.exit);
+});
--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const program     = require('commander');
 const term        = require('terminal-kit').terminal;
 const { version } = require('./package.json');
+const dump        = require('./lib/dump');
+const fs          = require('fs');
+const commands    = [];
+
+// Argument list.
+const argument = process.argv.slice(2, process.argv.length);
+if (!process.argv[2]) process.argv.push('all');
 
 /**
  * Command-line interface.
@@ -8,19 +15,100 @@ const { version } = require('./package.json');
 program
   .version(version)
   .name('sys-get')
-  .command('all', 'Displays general infromation about the system.', { isDefault: true })
-  .command('cpu', 'Displays information about the system CPU.')
-  .command('memory', 'Displays information about the current memory usage.')
-  .command('network', 'Displays information on the system network interfaces.')
-  .command('os', 'Displays information on the operating system.')
-  .command('processes', 'Displays information on the currently running processes.')
-  .command('storage', 'Display information on the storage devices.')
+  .option('all', 'Displays all the system information.')
+  .option('cpu', 'Displays information about the system CPU.')
+  .option('memory', 'Displays information about the current memory usage.')
+  .option('network', 'Displays information on the system network interfaces.')
+  .option('os', 'Displays information on the operating system.')
+  .option('processes', 'Displays information on the currently running processes.')
+  .option('storage', 'Display information on the storage devices.')
   .command('serve [namespace] [endpoint]', 'Serves system information on a local IPC interface.')
   .command('remote [namespace] [endpoint]', 'Displays system information by connecting to a remote IPC agent.')
+  .option('-l, --live', 'Specify this option if you would like to get system information in real-time.')
+  .option('-r, --refresh-rate <rate>', 'Specify after how many time, in milliseconds, the display should be refreshed.')
   .parse(process.argv);
 
+/**
+ * The application refresh rate.
+ */
+const rate = program.refreshRate || (2 * 1000);
+
+/**
+ * Exits the application by displaying the
+ * program usage.
+ */
+const fail = () => {
+  program.outputHelp();
+  process.exit(-1);
+};
+
+/**
+ * Building the `commands` array.
+ */
+fs.readdirSync('./lib').forEach((file) => commands.push(file.replace(/\.[^/.]+$/, '')));
+
+// If `commander` handled a command, we stop.
+if (program.args.length > 0) {
+  return;
+}
+
+/**
+ * Displays the system information associated with the
+ * given array of topics.
+ * @param {*} list the array of topics to display information
+ * about.
+ */
+const display = (list) => {
+  const array = [];
+
+  // Loads commands in an array of promises.
+  const load = (cmds) => {
+    cmds.forEach((command) => {
+      if (commands.indexOf(command) < 0) return;
+      const provider = require(`./lib/${command}`);
+      if (provider.get) {
+        array.push(provider.get().then((res) => ({ res, command })));
+      }
+    });
+  };
+
+  load(list);
+
+  // If no commands were found, we stop.
+  if (!array.length) load(commands);
+
+  // Displaying the gathered information.
+  return Promise.all(array).then((results) => results.forEach((o, idx) => {
+    dump[o.command](o.res);
+    idx < results.length - 1 && console.log();
+  }));
+};
+
+/**
+ * Called on each refresh tick in `live` mode.
+ */
+const tick = () => {
+  term.resetScrollingRegion();
+  display(argument).then(() => setTimeout(tick, rate));
+};
+
+// Displaying informations.
+display(argument).then(() => {
+  // Showing real-time information in case the `live`
+  // argument is specified.
+  if (program.live) {
+    term.clear().hideCursor();
+    setTimeout(tick, rate);
+  }
+});
 
 /**
  * Resetting the terminal on exit.
  */
-process.on('SIGINT', term.reset);
+process.on('SIGINT', () => {
+  term.reset();
+  if (process.rawListeners('SIGINT').length === 1) {
+    // If no other listeners are executing, we close the program.
+    process.exit(0);
+  }
+});
